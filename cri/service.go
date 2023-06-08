74b6b45e3189b54267f388bc5fa02a99bb2c8c29
zhaoshihan <zhaosh1997@gmail.com>
2023-06-08 16:41:01 +0200

    fix: use format string

--- cri/service.go
+++ cri/service.go
@@ -77,7 +77,7 @@ func (s *Service) CreateContainer(ctx context.Context, r *criapi.CreateContainer
 }
 
 func (s *Service) RemoveContainer(ctx context.Context, r *criapi.RemoveContainerRequest) (*criapi.RemoveContainerResponse, error) {
-	log.Debug("RemoveContainer, context: %v, request: %v", ctx, r)
+	log.Debugf("RemoveContainer, context: %v, request: %v", ctx, r)
 	return s.serv.RemoveContainer(ctx, r)
 }
 
